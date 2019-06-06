efbbaa7aa260cba34424519902078a161583c727
Luther Monson <luther.monson@gmail.com>
2019-06-06 11:04:11 -0700

    removed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "node_command" {
-  value = "${rancher2_cluster.rke.cluster_registration_token.0.node_command}"
-}
+# output "node_command" {
+#   value = "${rancher2_cluster.rke.cluster_registration_token.0.node_command}"
+# }
 
-output "kube_config" {
-  value = "${rancher2_cluster.rke.kube_config}"
-}
+# output "kube_config" {
+#   value = "${rancher2_cluster.rke.kube_config}"
+# }
